a9bba8fa4735dd05ddf6c46b307d3753bc4f94e3
Christian Gonzalez <cgonzalez233@yahoo.com>
2022-03-07 16:16:07 -0500

    organized files in packages

---
+++ src/main/java/ManagementServlets/MLoginServlet.java
@@ -0,0 +1,41 @@
+package ManagementServlets;
+
+import ManagementDao.IManagerDao;
+import ManagementDao.ManagerDaoFactory;
+import UserDao.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MLoginServlet extends HttpServlet {
+
+    IManagerDao managerDao = ManagerDaoFactory.getManagerDao();
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        User manager;
+        String uname = request.getParameter("username");
+        String pass = request.getParameter("password");
+
+        manager = managerDao.mLogin(uname, pass);
+
+        if (manager == null){
+            out.println("<p style=\"color: red\">You're Not a Manager</p>");
+            request.getRequestDispatcher("index.html").include(request, response);
+        }else {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("_susername",manager.getUsername());
+
+            request.getRequestDispatcher("navbar.html").include(request, response);
+            out.println("<h1>Welcome "+ manager.getName() +"</h1>");
+            request.getRequestDispatcher("managerPage.html").include(request, response);
+        }
+    }
+
+}
